18c8f5170f970a264159abb209adbb619d792ca4
Nirajan Basnet <er.nirajan.basnet@gmail.com>
2019-11-22 13:47:00 +0545

    Himal Doc homepage UI

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	theme: {
+		fontFamily: {
+			body: ["Source Sans Pro", "sans-serif"]
+		},
+		colors: {
+			"gray-lighter": "#f1f2f2",
+			"gray-light": "#e6e7e8",
+			"gray-default": "#a7a9ac",
+			"gray-dark": "#58595b",
+			"gray-darker": "#333233",
+			white: "#ffffff",
+			blue: "#036cb6",
+			"blue-hover": "#004d8e"
+		},
+		spacing: {
+			"8": "8px",
+			"12": "12px",
+			"16": "16px",
+			"24": "24px",
+			"32": "32px",
+			"40": "40px",
+			"48": "48px",
+			"60": "60px",
+			"90": "90px",
+			"120": "120px",
+			"470": "470px"
+		},
+		extend: {}
+	},
+	variants: {},
+	plugins: []
+};
